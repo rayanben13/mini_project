b44b979495264671850c4b8dc1153986a22b7cb7
rayan ben13 <rayanbensalem444@gmail.com>
2026-05-23 19:06:24 +0200

    modify

--- front/src/components/renderState.tsx
+++ front/src/components/renderState.tsx
@@ -5,44 +5,45 @@ import AddedStudyListCard from "./studyList/addedStudyListCard";
 import StudyListSkeleton from "./studyList/StudyListSkeleton";
 
 interface RenderStateProps {
-    readonly isLoading: boolean;
-    readonly data: any;
-    readonly showSave?: boolean;
+  readonly isLoading: boolean;
+  readonly data: any;
+  readonly showSave?: boolean;
 }
 
-
 export default function RenderState({
-    isLoading,
-    data,
-    showSave = false
+  isLoading,
+  data,
+  showSave = false,
 }: RenderStateProps) {
-    const router = useRouter();
-    const pathname = usePathname();
+  const router = useRouter();
+  const pathname = usePathname();
+  console.log("RenderState data:", data);
 
+  if (isLoading) return <StudyListSkeleton />;
 
-    if (isLoading) return <StudyListSkeleton />;
+  if (data?.data?.length) {
+    return data.data.map((item: any) => (
+      <div
+        key={item.id_stuList}
+        onClick={(e) => {
+          if ((e.target as HTMLElement).closest("[data-stop]")) return;
+          router.push(
+            `${pathname.includes("study-list") ? `/dashboard/study-list/${item.id_stuList}` : `/dashboard/study_list/${item.id_stuList}`}`,
+          );
+        }}
+      >
+        <AddedStudyListCard
+          id={item.id_stuList}
+          title={item.name}
+          files={item.count_files}
+          userName={item.users?.fullname || "Unknown"}
+          likes={item.count_likes || 0}
+          showSave={showSave}
+          isAlreadySaved={item.isSaved}
+        />
+      </div>
+    ));
+  }
 
-    if (data?.data?.length) {
-        return data.data.map((item: any) => (
-            <div
-                key={item.id_stuList}
-                onClick={(e) => {
-                    if ((e.target as HTMLElement).closest("[data-stop]")) return;
-                    router.push(`${pathname.includes('study-list') ? `/dashboard/study-list/${item.id_stuList}` : `/dashboard/study_list/${item.id_stuList}`}`)
-                }}
-            >
-                <AddedStudyListCard
-                    id={item.id_stuList}
-                    title={item.name}
-                    files={item.count_files}
-                    userName={item.users?.fullname || "Unknown"}
-                    likes={item.count_likes || 0}
-                    showSave={showSave}
-                    isAlreadySaved={item.isSaved}
-                />
-            </div>
-        ));
-    }
-
-    return null;
+  return null;
 }
